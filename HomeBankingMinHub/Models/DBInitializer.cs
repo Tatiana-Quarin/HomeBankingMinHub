54c35691927d788816132fa3ef0d294f6d4c24b4
Tatiana Quarin <tatiana.quarin@gmail.com>
2023-07-25 11:11:56 -0300

    Agregar archivos de proyecto.

---
+++ HomeBankingMinHub/Models/DBInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace HomeBankingMinHub.Models
+{
+    public class DBInitializer
+    {
+        private static Client client;
+
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client
+                    {
+                        FirstName = "Victor",
+                        LastName = "Coronado",
+                        Email = "vcoronado@gmail.com",
+                        Password = "123456",
+                    },
+                    new Client
+                    {
+                        FirstName = "Tatiana",
+                        LastName = "Quarin",
+                        Email = "tatiana.quarin@gmail.com",
+                        Password = "12345",
+                    },
+                };
+
+                foreach (var client in clients)
+                {
+                    context.Clients.Add(client);
+                }
+            }
+             context.SaveChanges();
+        }
+    }
+}
+
+
+
